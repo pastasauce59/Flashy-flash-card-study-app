1928bf3ac0db6d6014e41365dd046c69bae43656
Michael Primavera <pastasauce59@gmail.com>
2023-04-20 23:44:26 -0400

    Created and configured window with TKinter, created a canvas using the card_front.png image, created right and wrong buttons using png images from images folder, placed all on the window.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title('Flashy - Flashcard app')
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
+front_card_img = PhotoImage(file='images/card_front.png')
+canvas.create_image(400, 263, image=front_card_img)
+canvas.grid(column=0, row=0, columnspan=2)
+
+r_btn_img = PhotoImage(file='images/right.png')
+right_button = Button(image=r_btn_img, highlightthickness=0)
+right_button.grid(column=1, row=1)
+
+w_btn_img = PhotoImage(file='images/wrong.png')
+wrong_button = Button(image=w_btn_img, highlightthickness=0)
+wrong_button.grid(column=0, row=1)
+
+window.mainloop()
