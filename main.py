0d0744f7baa923c63814cbe81736d9819ad83042
Michael Primavera <pastasauce59@gmail.com>
2023-04-22 22:52:14 -0400

    Created language and word labels, opend up french_words.csv with pandas to use data in the word label, placed both labels on window with place() specific x & y coordinates.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from tkinter import *
+import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+with open('data/french_words.csv') as file:
+    data = pandas.read_csv(file)
+    print(data['French'][0])
 
 window = Tk()
 window.title('Flashy - Flashcard app')
@@ -18,5 +23,11 @@ right_button.grid(column=1, row=1)
 w_btn_img = PhotoImage(file='images/wrong.png')
 wrong_button = Button(image=w_btn_img, highlightthickness=0)
 wrong_button.grid(column=0, row=1)
+
+language_label = Label(text='French', bg='white', fg='black', font=('Ariel', 40, 'italic'), justify='center')
+language_label.place(x=300, y=150)
+
+word_label = Label(text=data['French'][0], bg='white', fg='black', font=('Ariel', 60, 'bold'), justify='center')
+word_label.place(x=300, y=263)
 
 window.mainloop()
