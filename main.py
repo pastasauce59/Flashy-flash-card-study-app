57cba393915a77694bf63a733fce8e98bd377d84
Michael Primavera <pastasauce59@gmail.com>
2023-04-23 22:59:06 -0400

    Created dataframe (df) from frenech_words.csv, created next_card function to call a random card from the french words data frame and then change the word whenever the right or wrong button is pressed.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from tkinter import *
 import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-with open('data/french_words.csv') as file:
-    data = pandas.read_csv(file)
-    print(data['French'][0])
+data = pandas.read_csv('data/french_words.csv')
+# print(data['French'][0])
+df = data.to_dict(orient='records')
+# print(df[0]['French'])
 
+def next_card():
+    current_card = random.choice(df)
+    word = current_card['French']
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=word)
+
+   
 window = Tk()
 window.title('Flashy - Flashcard app')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -14,17 +23,18 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card_img = PhotoImage(file='images/card_front.png')
 canvas.create_image(400, 263, image=front_card_img)
-canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'), fill='black')
-canvas.create_text(400, 263, text='Word', font=('Ariel', 60, 'bold'), fill='black')
+card_title = canvas.create_text(400, 150, text='Title', font=('Ariel', 40, 'italic'), fill='black')
+card_word = canvas.create_text(400, 263, text='Word', font=('Ariel', 60, 'bold'), fill='black')
 canvas.grid(column=0, row=0, columnspan=2)
 
 r_btn_img = PhotoImage(file='images/right.png')
-right_button = Button(image=r_btn_img, highlightthickness=0)
+right_button = Button(image=r_btn_img, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
 w_btn_img = PhotoImage(file='images/wrong.png')
-wrong_button = Button(image=w_btn_img, highlightthickness=0)
+wrong_button = Button(image=w_btn_img, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
+next_card()
 
 window.mainloop()
